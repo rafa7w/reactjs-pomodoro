c3d956c63b3d2cb8ab95a3f49f495ed26fc50e3f
Rafael William <rafael.fim@hotmail.com>
2023-07-26 14:56:44 -0300

    page: history

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,48 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Uma tarefa qualquer</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa qualquer</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa qualquer</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+            <tr>
+              <td>Uma tarefa qualquer</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Em andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
