212d81a7739c6b711105d397edbfe15435382f7d
Rafael William <rafael.fim@hotmail.com>
2023-07-26 15:36:47 -0300

    status colors

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -20,25 +20,41 @@ export function History() {
               <td>Uma tarefa qualquer</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">
+                  Concluído
+                </Status>
+              </td>
             </tr>
             <tr>
               <td>Uma tarefa qualquer</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="yellow">
+                  Em andamento
+                </Status>
+              </td>
             </tr>
             <tr>
               <td>Uma tarefa qualquer</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="red">
+                  Interrompido
+                </Status>
+              </td>
             </tr>
             <tr>
               <td>Uma tarefa qualquer</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="yellow">
+                  Em andamento
+                </Status>
+              </td>
             </tr>
           </tbody>
         </table>
